dbc1dce4b80dace0178e1f5840d26e59c5804dd6
Shari Fedak <90287794+sharifedak12@users.noreply.github.com>
2022-06-10 15:56:32 -0400

    Wave 6 complete, ready to deploy

--- src/index.js
+++ src/index.js
@@ -49,6 +49,21 @@ const changeCity = () => {
     cityName.textContent = city.value;
 };
 
+const reset = () => {
+    const city = document.getElementById('city');
+    const cityName = document.getElementById('cityName');
+    city.value = 'Baltimore';
+    cityName.textContent = 'Baltimore';
+    state.temp = 70;
+    state.tempColour = 'black';
+    temp.textContent = `${state.temp}°F`;
+    temp.style.color = state.tempColour;
+    const skyBackground = document.getElementById('sky');
+    skyBackground.style.backgroundImage = `url('../assets/bg_sunny.png')`;
+    const foreground = document.getElementById('weatherGarden');
+    foreground.src = 'assets/foreground_spring.png';
+};
+
 const registerEventHandlers = () => {
     const up = document.getElementById('up');
     up.addEventListener('click', increaseTemp);
@@ -58,6 +73,60 @@ const registerEventHandlers = () => {
     sky.addEventListener('change', changeSky);
     const city = document.getElementById('city');
     city.addEventListener('change', changeCity);
+    const getWeatherButton = document.getElementById('getWeatherButton');
+    getWeatherButton.addEventListener('click', getLocation);
+    const resetButton = document.getElementById('resetButton');
+    resetButton.addEventListener('click', reset);
 };
 
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
+
+const convertKelvinToFahrenheit = (kelvin) => {
+    return Math.round((kelvin - 273.15) * 1.8 + 32);
+};
+
+const getLocation = () => {
+    const city = document.getElementById('cityName');
+    const cityName = city.textContent;
+    axios
+        .get('http://localhost:5000/location', {
+            params: {
+                q: cityName,
+            },
+        })
+        .then((response) => {
+            const latitude = response.data[0].lat;
+            const longitude = response.data[0].lon;
+            getWeather(latitude, longitude);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+const getWeather = (latitude, longitude) => {
+    axios
+        .get('http://localhost:5000/weather', {
+            params: {
+                lat: latitude,
+                lon: longitude,
+            },
+        })
+        .then((response) => {
+            const weather = response.data;
+            state.temp = convertKelvinToFahrenheit(weather.current.temp);
+            updateTemp();
+            const skyBackground = document.getElementById('sky');
+            if (weather.current.weather[0].main === 'Clouds') {
+                skyBackground.style.backgroundImage = `url('../assets/bg_cloudy.png')`;
+            } else if (weather.current.weather[0].main === 'Clear') {
+                skyBackground.style.backgroundImage = `url('../assets/bg_sunny.png')`;
+            } else if (weather.current.weather[0].main === 'Rain') {
+                skyBackground.style.backgroundImage = `url('../assets/bg_raining.png')`;
+            } else if (weather.current.weather[0].main === 'Snow') {
+                skyBackground.style.backgroundImage = `url('../assets/bg_snowing.png')`;
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
